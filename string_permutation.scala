5c90d81c7c1f9619d7671a346814f42506c26af3
siddhusalvi <siddheshsalvi525@gmail.com>
2020-02-01 11:34:17 +0530

    CREATED: string permutation program

---
+++ string_permutation.scala
@@ -0,0 +1,10 @@
+/*
+Filename: string_permutation
+Created: Siddhesh salvi
+Change history:1.2.2020 / Siddhhesh Salvi
+1. Write static functions to return all permutations of a String using iterative method and
+Recursion method. Check if the arrays returned by two string functions are equal.
+ */
+object string_permutation {
+
+}
